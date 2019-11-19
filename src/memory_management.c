de2114f0c234e049da651b2cac5d55aa28f99a67
Jasmine Tarbard <sc18jt@localhost.localdomain>
2019-11-19 15:33:30 +0000

    Implemented a basic doubly linked list, added behaviour to handle _malloc calls with a size of 0 and added a basic sbrk call.

--- src/memory_management.c
+++ src/memory_management.c
@@ -1,12 +1,29 @@
 #include "memory_management.h"
 #include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
+//Doubly linked list: stores block structure
+//TODO: expand upon basic linked list with necessary variables
+struct Node {
+    int size_t;
+
+    struct Node* prev;
+    struct Node* next;
+};
+struct Node* head;
+
+//TODO: map requirements on paper to build from
 void * _malloc(size_t size){
-	
-	return NULL;
-}
 
+    if(size == 0) {
+        return NULL;
+    }
+    else {
+        return sbrk(size);
+    }
+}
+//TODO: map requirements on paper to build from
 void _free(void * ptr){
 
 }
