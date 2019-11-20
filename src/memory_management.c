9579acf5eacc3055d5ea7cb643aa3a7c7b2d4c8c
Jasmine Tarbard <sc18jt@leeds.ac.uk>
2019-11-20 00:47:31 +0000

    Fully implemented linked list, added basic _free function behaviour, created a function for making block headers.

--- src/memory_management.c
+++ src/memory_management.c
@@ -5,26 +5,84 @@
 
 //Doubly linked list: stores block structure
 //TODO: expand upon basic linked list with necessary variables
-struct Node {
-    int size_t;
+typedef struct block {
+    int free_t; //status of to whether the block is populated
 
-    struct Node* prev;
-    struct Node* next;
-};
-struct Node* head;
+    int size_t; //remaining space in block
+    char* address_t; //holder for sbrk return address
 
-//TODO: map requirements on paper to build from
+    //struct block* prev;
+    struct block* next; //previous block
+} block_t;
+
+//Declare structs
+block_t *head = NULL;
+block_t *block_head;
+
+block_t *pointer;
+block_t *new_pointer;
+
+void new_block(size_t size){
+    block_head = sbrk(sizeof(block_head));
+    if(size > 0) { //populated block
+        block_head->free_t = 0;
+    }
+    else{ //freed block
+        block_head->free_t = 1;
+    }
+    block_head->size_t = 4000 - sizeof(block_head);
+    block_head->address_t = sbrk(0);
+    //TODO: next - consider that insertion may happen in the middle or end
+    if(pointer == head){
+        block_head->next = head;
+        head = block_head;
+    }
+    else{
+        block_head->next = pointer->next;
+        pointer->next = block_head;
+    }
+
+    return NULL;
+}
+
 void * _malloc(size_t size){
+    //Initialise head
+    if(head == NULL){
+        //create first block header
+        block_head = sbrk(sizeof(block_head));
+        block_head->free_t = 0;
+        block_head->size_t = 4000 - sizeof(block_head);
+        block_head->address_t = sbrk(0);
+        block_head->next = head;
+        //update head
+        head = block_head;
+    }
+    if(size != 0){
+        pointer = head;
 
-    if(size == 0) {
+        //Todo: Fix loop, currently breaking tests with segmentation fault
+//        while(pointer){
+//
+//            if(pointer->free_t == 1){
+//                new_block(size);
+//            }
+//            else if(pointer->size_t >= size){
+//                new_block(size);
+//            }
+//
+//            pointer = pointer->next;
+//        }
+
+        new_block(size);
+        return sbrk(0);
+    }
+    else{
         return NULL;
     }
-    else {
-        return sbrk(size);
-    }
-}
-//TODO: map requirements on paper to build from
-void _free(void * ptr){
 
+}
+
+void _free(void * ptr){
+    sbrk(-sizeof(ptr));
 }
 
